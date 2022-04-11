f57bfd378b742165a235a08cf36ace023eb34a8f
mrshmllow <marshycity@gmail.com>
2022-04-12 09:54:36 +1000

    add renogare font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["renogare", ...defaultTheme.fontFamily.sans],
+        renogare: ["renogare"],
+      },
+    },
   },
   plugins: [],
 };
